c27e67a52229a6347d2675675c1805b177f26c7d
Laurence Sadler <sirlaurence@protonmail.com>
2022-06-15 12:45:19 -0700

    Update ubuntu-plasticscm-intsall.sh

--- ubuntu-plasticscm-intsall.sh
+++ ubuntu-plasticscm-intsall.sh
@@ -12,12 +12,12 @@ echo "deb https://www.plasticscm.com/plasticrepo/stable/ubuntu/ ./" | sudo tee /
 wget https://www.plasticscm.com/plasticrepo/stable/ubuntu/Release.key -O - | sudo apt-key add -
 sudo apt-get update
 
-#https://plasticscmsupport.zendesk.com/hc/en-us/articles/360013103159-How-to-Install-Plastic-GUI-in-Ubuntu-19-10-and-later-
-#If the apt-update is missing a key run:
-#Sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 3B4FE6ACC0B21F32
+# https://plasticscmsupport.zendesk.com/hc/en-us/articles/360013103159-How-to-Install-Plastic-GUI-in-Ubuntu-19-10-and-later-
+# If the apt-update is missing a key run:
+# sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 3B4FE6ACC0B21F32
 
-#If behind firewall:
-Sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 3B4FE6ACC0B21F32
+# If behind firewall:
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 3B4FE6ACC0B21F32
 
 # =======================================
 # 2. Installing Plastic SCM Cloud Edition
