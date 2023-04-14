f37249a7cb183f67305bbe10b153fd4c40a14ad6
Laurence Sadler <sirlaurence@protonmail.com>
2023-04-14 12:41:49 -0700

    Update and rename my-install-common.sh to common-install.sh

---
+++ common-install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#run this with sudo bash {location}
+
+#=================
+# INITIAL UPDATES
+#=================
+apt updates
+apt upgrades -y
+
+#===========
+# INSTALLS
+#===========
+
+#steam  
+apt install steam -y
+
+
+# >>>>>> FLATPAKS
+
+# brave
+flatpak install flathub com.brave.Browser
+
+# bottles - for gog
+flatpak install flathub com.usebottles.bottles
+
+# discord
+flatpak install flathub com.discordapp.Discord
+
+# Spotify 
+flatpak install flathub com.spotify.Client
+
+#================
+# FINAL CLEAN UP
+#================
+apt-get purge firefox -y
+
+# apt updates
