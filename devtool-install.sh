d6c48c06748979d5bd0c1c5dfb240d97a6ec162f
Laurence Sadler <sirlaurence@protonmail.com>
2023-04-15 13:15:44 -0700

    Update devtool-install.sh
    added git-lfs install

--- devtool-install.sh
+++ devtool-install.sh
@@ -26,3 +26,7 @@ rm -f packages.microsoft.gpg
 apt install apt-transport-https -y
 apt update
 apt install code -y
+
+# GIT LFS
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+apt install git-lfs
