5016fbcc33e17012deabc31b8a5f6180bcf71ceb
Laurence Sadler <sirlaurence@protonmail.com>
2023-04-15 09:40:36 -0700

    Update devtool-install.sh

--- devtool-install.sh
+++ devtool-install.sh
@@ -15,4 +15,14 @@ wget -qO - https://hub.unity3d.com/linux/keys/public | sudo apt-key add -
 apt update 
 apt install unityhub -y
 
+# VSCODE 
+# web link: https://code.visualstudio.com/docs/setup/linux
+apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
 
+apt install apt-transport-https -y
+apt update
+apt install code -y
