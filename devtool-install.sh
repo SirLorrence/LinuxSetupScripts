0ec74a4680f1904dce4c4eb5bb3b7ff20dd53e55
Laurence Sadler <sirlaurence@protonmail.com>
2023-04-15 08:53:47 -0700

    Update devtool-install.sh

--- devtool-install.sh
+++ devtool-install.sh
@@ -2,7 +2,7 @@
 
 # MONO (for unity and unreal) 
 # web link: https://www.monodevelop.com/download/
-apt install apt-transport-https dirmngr
+apt install apt-transport-https dirmngr -y
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
 apt update
